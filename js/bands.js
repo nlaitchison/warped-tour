4903024dce9bdaa999c143d64e697de39779bd35
nlaitchison <nlaitchison@gmail.com>
2015-06-25 23:32:47 -0400

    added files

---
+++ js/bands.js
@@ -0,0 +1,463 @@
+var warpedTour = { "bands": [
+
+	{
+		"band" : "Asking Alexandria",
+		"stage" : "SHARK STAGE"
+	},
+	{
+		"band" : "August Burns Red",
+		"stage" : "SHARK STAGE"
+	},
+	{
+		"band" : "Black Veil Brides",
+		"stage" : "SHARK STAGE"
+	},
+	{
+		"band" : "blessthefall",
+		"stage" : "SHARK STAGE"
+	},
+	{
+		"band" : "Four Year Strong",
+		"stage" : "SHARK STAGE"
+	},
+	{
+		"band" : "H2O",
+		"stage" : "SHARK STAGE"
+	},
+	{
+		"band" : "Motion City Soundtrack",
+		"stage" : "SHARK STAGE"
+	},
+	{
+		"band" : "Never Shout Never",
+		"stage" : "SHARK STAGE"
+	},
+	{
+		"band" : "Simple Plan",
+		"stage" : "SHARK STAGE"
+	},
+	{
+		"band" : "The Wonder Years",
+		"stage" : "SHARK STAGE"
+	},
+	{
+		"band" : "Attila",
+		"stage" : "UNICORN STAGE"
+	},
+	{
+		"band" : "Family Force 5",
+		"stage" : "UNICORN STAGE"
+	},
+	{
+		"band" : "Man Overboard",
+		"stage" : "UNICORN STAGE"
+	},
+	{
+		"band" : "Memphis May Fire",
+		"stage" : "UNICORN STAGE"
+	},
+	{
+		"band" : "Miss May I",
+		"stage" : "UNICORN STAGE"
+	},
+	{
+		"band" : "Pierce The Veil",
+		"stage" : "UNICORN STAGE"
+	},
+	{
+		"band" : "We Came As Romans",
+		"stage" : "UNICORN STAGE"
+	},
+	{
+		"band" : "'68",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Beartooth",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Being As An Ocean",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Crossfaith",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Escape The Fate",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Fit For A King",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Hundredth",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "I Killed The Prom Queen",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Mallory Knox",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Our Last Night",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Senses Fail",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Silverstein",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "The Amity Affliction",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "While She Sleeps",
+		"stage" : "MONSTER ENERGY"
+	},
+	{
+		"band" : "Bebe Rexha",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "Icon For Hire",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "James Morris",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "Mod Sun",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "Moose Blood",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "Neck Deep",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "New Years Day",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "PUP",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "PVRIS",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "Riff Raff",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "Set It Off",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "This Wild Life",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "Transit",
+		"stage" : "JOURNEYS LEFT FOOT"
+	},
+	{
+		"band" : "As It Is",
+		"stage" : "JOURNEYS RIGHT FOOT"
+	},
+	{
+		"band" : "Beautiful Bodies",
+		"stage" : "JOURNEYS RIGHT FOOT"
+	},
+	{
+		"band" : "Citizen",
+		"stage" : "JOURNEYS RIGHT FOOT"
+	},
+	{
+		"band" : "Handguns",
+		"stage" : "JOURNEYS RIGHT FOOT"
+	},
+	{
+		"band" : "Hands Like Houses",
+		"stage" : "JOURNEYS RIGHT FOOT"
+	},
+	{
+		"band" : "Juliet Simms",
+		"stage" : "JOURNEYS RIGHT FOOT"
+	},
+	{
+		"band" : "Lee Corey Oswald",
+		"stage" : "JOURNEYS RIGHT FOOT"
+	},
+	{
+		"band" : "Matchbook Romance",
+		"stage" : "JOURNEYS RIGHT FOOT"
+	},
+	{
+		"band" : "Metro Station",
+		"stage" : "JOURNEYS RIGHT FOOT"
+	},
+	{
+		"band" : "Night Riots",
+		"stage" : "JOURNEYS RIGHT FOOT"
+	},
+	{
+		"band" : "Alive Like Me",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Alvarez Kings",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Baby Baby",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Candy Hearts",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Carousel Kings",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Emarosa",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Have Mercy",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Jule Vera",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Kaya Stewart",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Knuckle Puck",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Major League",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Palisades",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Sgt. Corrin Campbell presented by Army Musical Outreach",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Shiragirl",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Slaves",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Stacked Like Pancakes",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Sumo Cyco",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Svetlanas",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "The Kenneths",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "Trophy Eyes",
+		"stage" : "HARD ROCK KEVIN SAYS"
+	},
+	{
+		"band" : "A+ Dropouts",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "Ascot Royals",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "BoyMeetsWorld",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "False Puppet",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "Isles",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "Night Argent",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "Rivers Monroe",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "Seaway",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "Sirens and Sailors",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "Strangers You Know",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "The Bunny The Bear",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "The Dirty Nil",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "The Karma Killers",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "The Relapse Symphony",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "The Republic Of Wolves",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "Youth In Revolt",
+		"stage" : "ERNIE BALL"
+	},
+	{
+		"band" : "Aaron West and The Roaring Twenties",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "American Opera",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "Brian Marquis",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "Buttons",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "Grey Gordon",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "Koji",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "Macy Kate",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "Meghann Wright",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "Moose Blood",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "Onward, Etc.",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "SayWeCanFly",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "Speak Low If You Speak Love",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "The Ready Set",
+		"stage" : "FULL SAIL UNIVERSITY ACOUSTIC BASEMENT"
+	},
+	{
+		"band" : "Black Boots",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "Born Cages",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "ChrisB",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "Drama Club",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "Koo Koo Kanga Roo",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "Kosha Dillz",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "Le Castle Vania",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "M4Sonic",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "MC Lars",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "New Beat Fund",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "Splitbreed",
+		"stage" : "BEATPORT"
+	},
+	{
+		"band" : "TAT",
+		"stage" : "BEATPORT"
+	}
+]};
---
+++ js/main.js
@@ -0,0 +1,157 @@
+
+(function($) {
+
+	function sortList() {
+
+	    var ul = $('#band-list');
+	    var li = ul.children("li");
+
+	    li.detach().sort(function(a, b) {
+	        var date1 = new Date (new Date().toDateString() + ' ' + $(a).children('span.time').text());
+	        var date2 = new Date (new Date().toDateString() + ' ' + $(b).children('span.time').text());
+	        console.log(date1, date2);
+	        var result = (date1 < date2) ? -1 : (date1 > date2) ? 1 : 0;
+	        return result;
+	    });
+
+	    ul.append(li);
+	}
+
+	jQuery('#add-band').on('click', function(){
+
+		var item = {
+			'hour' : jQuery('#hour').val(),
+			'min' : jQuery('#minute').val(),
+			'band' : jQuery('#band').val(),
+			'stage' : jQuery('#stage').val(),
+		};
+
+		jQuery('#hour').val('');
+		jQuery('#minute').val('');
+		jQuery('#band').val('');
+		jQuery('#stage').val('');
+		$('#s-results').empty();
+
+		var t = '';
+		if( item.hour == 11 )
+			var t = 'am';
+		else
+			var t = 'pm';
+
+		jQuery('#band-list').append('<li><span class="time">'+item.hour+':'+item.min+' '+t+' </span><span class="band">'+item.band+' </span><span class="stage">'+item.stage+' </span></li>')
+
+		sortList();
+
+	});
+
+// ---------------------------------------------------------------------------------------------------------------------
+// TIPUE DROP
+// ---------------------------------------------------------------------------------------------------------------------
+/*
+Tipue drop 4.0
+Copyright (c) 2014 Tipue
+Tipue drop is released under the MIT License
+http://www.tipue.com/drop
+*/
+	$.fn.tipuedrop = function(options) {
+
+		var set = $.extend( {
+		   'show'                   : 15,
+		   'speed'                  : 300,
+		   'newWindow'              : false,
+		   'mode'                   : 'static',
+		   'contentLocation'        : 'js/bands.json'
+		}, options);
+
+		return this.each(function() {
+
+			var tipuedrop_in = {
+			    bands: []
+			};
+
+			$.ajaxSetup({
+			    async: false
+			});
+
+			if (set.mode == 'json')
+			{
+			    $.getJSON(set.contentLocation,
+			         function(json)
+			         {
+			              tipuedrop_in = $.extend({}, json);
+			         }
+			    );
+			}
+
+			if (set.mode == 'static')
+			{
+			    tipuedrop_in = $.extend({}, warpedTour);
+			}
+
+			var keyCount = 0;
+
+			$(this).on('focus', function(e){
+				getTipuedrop($('#band'));
+			});
+
+			$(this).keyup(function(e)
+			{
+				if(e.keyCode == 8){
+					keyCount--;
+				}else{
+					keyCount++;
+				}
+				if(keyCount >1){
+					getTipuedrop($(this));
+				} else{
+					$('#s-results').empty();
+				}
+			});
+
+			$('body').on('click', '#s-results li', function(){
+				$('body').find('#band').val(jQuery(this).data('band'));
+				$('body').find('#stage').val(jQuery(this).data('stage'));
+			});
+
+			function getTipuedrop($obj)	{
+	            if ($obj.val()){
+					var c = 0;
+					var ci = 0;
+
+					for (var i = 0; i < tipuedrop_in.bands.length; i++){
+						var obj = '^' + $obj.val();
+	                    var pat = new RegExp(obj, 'i');
+						var data = tipuedrop_in.bands[i].band;
+
+						if(c < set.show){
+
+							if(data.search(pat) !== -1 ){
+
+								if (ci === 0)
+								{
+									var bnd = '';
+								}
+
+								bnd += '<li data-band="'+tipuedrop_in.bands[i].band+'" data-stage="'+tipuedrop_in.bands[i].stage+'">' + tipuedrop_in.bands[i].band + tipuedrop_in.bands[i].stage + '</li>';
+								c++;
+								ci++;
+
+							}else {
+								$('#s-results').html('<h2>No results found...</h2>');
+							}
+						}
+
+	                }
+	                if (c !== 0) {
+						$('#s-results').empty();
+						$('#s-results').html(bnd);
+					}
+	            } else{
+	            	$('#s-results').html('<h2>Start searching...</h2>');
+	            	keyCount = 0;
+	            }
+			}
+		});
+	};
+
+})(jQuery);
