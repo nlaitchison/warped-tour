ff6abc35665be658344d5180196e0205bc98cea4
Nicole Aitchison <nlaitchison@gmail.com>
2016-06-30 22:00:53 -0400

    Added 2016 bands

--- js/bands-2016.js
+++ js/bands-2016.js
@@ -1,1 +1,354 @@
+var warpedTour = { "bands": [
 
+	{
+		"band" : "American Authors",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Falling in Reverse",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Four Year Strong",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Good Charlotte",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Less Than Jake",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Neck Deep",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "New Found Glory",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Reel Big Fish",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Set It Off",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Sum 41",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "The Summer Set",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Tsunami Bomb",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Waka Flocka Flame",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "We The Kings",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+		{
+		"band" : "Yellowcard",
+		"stage" : "JOURNEYS LEFT FOOT STAGE"
+	},
+	{
+		"band" : "3OH!3",
+		"stage" : "JOURNEYS Right FOOT STAGE"
+	},
+	{
+		"band" : "Issues",
+		"stage" : "JOURNEYS Right FOOT STAGE"
+	},
+	{
+		"band" : "Mayday Parade",
+		"stage" : "JOURNEYS Right FOOT STAGE"
+	},
+	{
+		"band" : "Pepper",
+		"stage" : "JOURNEYS Right FOOT STAGE"
+	},
+	{
+		"band" : "Real Friends",
+		"stage" : "JOURNEYS Right FOOT STAGE"
+	},
+	{
+		"band" : "Sleeping with Sirens",
+		"stage" : "JOURNEYS Right FOOT STAGE"
+	},
+	{
+		"band" : "State Champs",
+		"stage" : "JOURNEYS Right FOOT STAGE"
+	},
+	{
+		"band" : "The Maine",
+		"stage" : "JOURNEYS Right FOOT STAGE"
+	},
+	{
+		"band" : "The Story So Far",
+		"stage" : "JOURNEYS Right FOOT STAGE"
+	},
+	{
+		"band" : "Tonight ALive",
+		"stage" : "JOURNEYS Right FOOT STAGE"
+	},
+	{
+		"band" : "Atreyu",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "Bullet For My Valentine",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "Crown the Empire",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "Every Time I Die",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "Ice Nine Kills",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "Motionless in White",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "The Color Morale",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "The Word Alive",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "Volumes",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "Wage War",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "Whitechapel",
+		"stage" : "MONSTER ENERGY NORTH"
+	},
+	{
+		"band" : "Chelsea Grin",
+		"stage" : "MONSTER ENERGY SOUTH"
+	},
+	{
+		"band" : "Coldrain",
+		"stage" : "MONSTER ENERGY SOUTH"
+	},
+	{
+		"band" : "Cruel Hand",
+		"stage" : "MONSTER ENERGY SOUTH"
+	},
+	{
+		"band" : "From Ashes to New",
+		"stage" : "MONSTER ENERGY SOUTH"
+	},
+	{
+		"band" : "Gideon",
+		"stage" : "MONSTER ENERGY SOUTH"
+	},
+	{
+		"band" : "In Hearts Wake",
+		"stage" : "MONSTER ENERGY SOUTH"
+	},
+	{
+		"band" : "Oceans Ate Alaska",
+		"stage" : "MONSTER ENERGY SOUTH"
+	},
+	{
+		"band" : "Veil of May",
+		"stage" : "MONSTER ENERGY SOUTH"
+	},
+	{
+		"band" : "Vanna",
+		"stage" : "MONSTER ENERGY SOUTH"
+	},
+	{
+		"band" : "Emarosa",
+		"stage" : "POSEIDON STAGE"
+	},
+	{
+		"band" : "I See Stars",
+		"stage" : "POSEIDON STAGE"
+	},
+	{
+		"band" : "Knuckle Puck",
+		"stage" : "POSEIDON STAGE"
+	},
+	{
+		"band" : "Masked Intruder",
+		"stage" : "POSEIDON STAGE"
+	},
+	{
+		"band" : "Prof",
+		"stage" : "POSEIDON STAGE"
+	},
+	{
+		"band" : "Roam",
+		"stage" : "POSEIDON STAGE"
+	},
+	{
+		"band" : "Secrets",
+		"stage" : "POSEIDON STAGE"
+	},
+	{
+		"band" : "The Heirs",
+		"stage" : "POSEIDON STAGE"
+	},
+	{
+		"band" : "The Interrupters",
+		"stage" : "POSEIDON STAGE"
+	},
+	{
+		"band" : "Young Guns",
+		"stage" : "POSEIDON STAGE"
+	},
+	{
+		"band" : "Against the Current",
+		"stage" : "CYCLOPS STAGE"
+	},
+	{
+		"band" : "Assuming We Survive",
+		"stage" : "CYCLOPS STAGE"
+	},
+	{
+		"band" : "BallyHoo!",
+		"stage" : "CYCLOPS STAGE"
+	},
+	{
+		"band" : "Chunk! No, Captain Chunk!",
+		"stage" : "CYCLOPS STAGE"
+	},
+	{
+		"band" : "Ghost Town",
+		"stage" : "CYCLOPS STAGE"
+	},
+	{
+		"band" : "Sykes",
+		"stage" : "CYCLOPS STAGE"
+	},
+	{
+		"band" : Teenage Bottlerocket"",
+		"stage" : "CYCLOPS STAGE"
+	},
+	{
+		"band" : "Too Close to Touch",
+		"stage" : "CYCLOPS STAGE"
+	},
+	{
+		"band" : "Waterparks",
+		"stage" : "CYCLOPS STAGE"
+	},
+	{
+		"band" : "Avion Roe",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Bad Seed Rising",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Beebs Featuring B.Lay",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Broadside",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Cane Hill",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Capsize",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Dash|Ten",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "First to Eleven",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Hail the Sun",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Like Pacific",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Marina City",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "More to Monroe",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Mother Feather",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Night Argent",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Old Wounds",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Palaye Royale",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Reckless Serenade",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Safe to Say",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Saywecanfly",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "Silent Planet",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "The New Low",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "The Pink Slips",
+		"stage" : "FULL SAIL STAGE"
+	},
+	{
+		"band" : "With Confidence",
+		"stage" : "FULL SAIL STAGE"
+	},
